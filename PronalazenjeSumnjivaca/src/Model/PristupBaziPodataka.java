390a063c707d9ba4cb8833cbe01addda8fcc1f99
Tanja Židić <tanja.zidic@fer.hr>
2016-12-29 11:35:43 +0100

    ispravljen upit za dokaze

--- PronalazenjeSumnjivaca/src/Model/PristupBaziPodataka.java
+++ PronalazenjeSumnjivaca/src/Model/PristupBaziPodataka.java
@@ -13,7 +13,9 @@ public class PristupBaziPodataka {
 	private static final String DB_CONNECTION = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7150265?useUnicode=yes&characterEncoding=UTF-8";	
 	private static final String DB_USER = "sql7150265";
 	private static final String DB_PASSWORD = "Xshx7bdSHe";
-	
+	private static List<String> vrati=new ArrayList<String>();
+	private static Connection dbConnection = null;
+	private static Statement statement = null;
 	
 	private static Connection getDBConnection() {
 		Connection dbConnection = null;
@@ -92,11 +94,12 @@ public class PristupBaziPodataka {
 
 	
 	public static List<Dokaz> vratiDokaze (Map<String, String> mapa) throws SQLException {
-		Connection dbConnection = null;
-		Statement statement = null;
+		//Connection dbConnection = null;
+		//Statement statement = null;
 		Context<Dokaz> dokazi=new Context<>(new Dokaz());
 		ArrayList<String> upit=dokazi.izgenerirajUpit(mapa);
 		String query = upit.get(0);
+		//System.out.println(query);
 		Integer brojDNA=Integer.parseInt(upit.get(1));
 		Integer brojKrvnihGrupa=Integer.parseInt(upit.get(2));
 		Integer brojOruzja=Integer.parseInt(upit.get(3));
@@ -110,36 +113,48 @@ public class PristupBaziPodataka {
 			statement=dbConnection.prepareStatement(query);
 			ResultSet rs = statement.executeQuery(query);
 			
-			while (rs.next()) {
-				
-				Dokaz dokaz=new Dokaz();
-				dokaz.setID(rs.getInt(1));
-				dokaz.setNaziv(rs.getString(2));
-				dokaz.setFotografija(rs.getString(3));
-				dokaz.setNazivSlucaja(rs.getString(4));
-				int i=5;
-				
-				if(brojKrvnihGrupa!=0){
-					for(int j=brojKrvnihGrupa;j>0;j--){
-						dokaz.addKrvnaGrupa(rs.getString(i));
-						i++;
+				while (rs.next()) {
+					Dokaz dokaz=new Dokaz();
+					dokaz.setID(rs.getInt(1));
+					dokaz.setNaziv(rs.getString(2));
+					dokaz.setFotografija(rs.getString(3));
+					dokaz.setNazivSlucaja(rs.getString(4));
+					//System.out.println("novi   "+ dokaz.getID());
+
+					if(!(vratiListuDokaza(dokaz.getID(),"KrvnaGrupa").isEmpty())) {
+						dokaz.addAllKrvnaGrupa(vrati);
+						vrati.removeAll(vrati);
 					}
-				}
-				if(brojDNA!=0){
-					for(int j=brojDNA;j>0;j--){
-						dokaz.addDNASekvenca(rs.getString(i));
-						i++;
-					}
-				}
-				if(brojOruzja!=0){
-					for(int j=brojOruzja;j>0;j--){
-						dokaz.addTipOruzja(rs.getString(i));
-						i++;
-					}
-				}
 				
-				listaDokaza.add(dokaz);
-			}
+					
+					
+					if(!(vratiListuDokaza(dokaz.getID(),"DNASekvenca").isEmpty())) {
+						dokaz.addAllDNASekvenca(vrati);
+						vrati.removeAll(vrati);
+					}
+					
+					if(!(vratiListuDokaza(dokaz.getID(),"OtisakPrsta").isEmpty())) {
+						
+						for(String s: vrati) 
+							dokaz.addOtisakPrsta(s);
+						vrati.removeAll(vrati);
+						
+					}
+					
+					
+					if(!(vratiListuDokaza(dokaz.getID(),"TipOružja").isEmpty())) {
+						
+						dokaz.addAllTipOruzja(vrati);
+						vrati.removeAll(vrati);
+					}
+
+					
+					
+					listaDokaza.add(dokaz);
+				}
+
+				
+		
 			return listaDokaza;
 		} catch (Exception e) {
 			System.out.println(e);
@@ -153,6 +168,73 @@ public class PristupBaziPodataka {
 				dbConnection.close();
 			}
 		}
+		return null;
+	}
+
+
+
+	private static List<String> vratiListuDokaza(Integer id, String atribut) {
+		//Connection dbConnection = null;
+		PreparedStatement preparedStatement = null;
+		String query=null;
+		List<String> lista=new ArrayList<>();
+		if(atribut!="TipOružja" && atribut!="OtisakPrsta") {
+			if(atribut=="KrvnaGrupa") {
+				lista.add(atribut);
+				lista.add("ListaKrvnihGrupaNaDokaznomMaterijalu");
+			}
+			if(atribut=="DNASekvenca") {
+				lista.add(atribut);
+				lista.add("ListaDNASekvenciNaDokaznomMaterijalu");
+			}
+			query = "SELECT " + lista.get(0) + ".naziv" + lista.get(0)
+			+ " FROM DokazniMaterijal"
+			+ " LEFT JOIN " + lista.get(1) + " ON DokazniMaterijal.brojDokaznogMaterijala = " + lista.get(1) + ".dokazniMaterijalID"
+			+ " LEFT JOIN " + lista.get(0) + " ON " + lista.get(1) + "." + lista.get(0) +"ID = " + lista.get(0) + "." +lista.get(0) +"ID"
+			+ " WHERE DokazniMaterijal.brojDokaznogMaterijala = ?";
+	
+		}
+		
+		else if(atribut=="OtisakPrsta") {
+				lista.add(atribut);
+				lista.add("ListaOtisakaPrstijuNaDokaznomMaterijalu");
+				query = "SELECT " + lista.get(0) + ".fotografijaURL" 
+				+ " FROM DokazniMaterijal"
+				+ " LEFT JOIN " + lista.get(1) + " ON DokazniMaterijal.brojDokaznogMaterijala = " + lista.get(1) + ".dokazniMaterijalID"
+				+ " LEFT JOIN " + lista.get(0) + " ON " + lista.get(1) + "." + lista.get(0) +"ID = " + lista.get(0) + "." +lista.get(0) +"ID"
+				+ " WHERE DokazniMaterijal.brojDokaznogMaterijala = ?";
+			}
+			
+		
+		else {
+			query = "SELECT TipOružja.nazivOružja"
+					+ " FROM DokazniMaterijal"
+					+ " LEFT JOIN ListaOružja ON DokazniMaterijal.brojDokaznogMaterijala = ListaOružja.brojDokaznogMaterijala"
+					+ " LEFT JOIN TipOružja ON ListaOružja.tipOružjaID = TipOružja.tipOružjaID"
+					+ " WHERE DokazniMaterijal.brojDokaznogMaterijala = ?";
+			
+		}
+		//System.out.println(query);
+		try {
+			//dbConnection = getDBConnection();
+			preparedStatement=dbConnection.prepareStatement(query);
+			preparedStatement.setString(1, id.toString());		
+			ResultSet rs =preparedStatement.executeQuery();
+			
+			while (rs.next()) {
+				vrati.add(rs.getString(1));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return vrati;
+	}
+
+
+
+	public String generirajSQLupit(Map<String, String> kombinacija) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
